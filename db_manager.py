ad2ef28ac7cb77d90102355ca02f3294276a6522
aminmshahnani <am.shahnani@gmail.com>
2023-02-05 21:55:10 +0330

    Clean data an put in database
    Clean data in csv file, get data from menu in csv, create databse, create tables and store data in database

--- db_manager.py
+++ db_manager.py
@@ -1,5 +1,5 @@
 import mysql.connector as db
-
+import csv
 
 
 db_host = "localhost"
@@ -36,3 +36,26 @@ cursor.execute("CREATE TABLE restaurant_info(restaurant_id INT AUTO_INCREMENT PR
 cursor.execute("CREATE TABLE address(address_id INT AUTO_INCREMENT PRIMARY KEY,city_id INT,restaurant_id INT,area VARCHAR(100),full_address TEXT,FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`),FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant_info` (`restaurant_id`))")
 cursor.execute("CREATE TABLE rating(rating_id INT AUTO_INCREMENT PRIMARY KEY,restaurant_id INT,rate VARCHAR(100),rate_count VARCHAR(100),FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant_info` (`restaurant_id`))")
 cursor.execute("CREATE TABLE menu(menu_id INT AUTO_INCREMENT PRIMARY KEY,restaurant_id INT, food_title VARCHAR(100),price VARCHAR(100),ingredient TEXT,FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant_info` (`restaurant_id`))")
+
+
+csv_city_column =[]
+
+
+
+sql_city = "INSERT INTO customers () VALUES (%s, %s)"
+val_city = ("John", "Highway 21")
+db.execute(sql_city, val_city)
+
+sql_restaurant_info = "INSERT INTO customers () VALUES (%s, %s)"
+val_restaurant_info = ("John", "Highway 21")
+db.execute(sql_restaurant_info, val_restaurant_info)
+
+sql_address = "INSERT INTO customers () VALUES (%s, %s)"
+val_address = ("John", "Highway 21")
+db.execute(sql_address, val_address)
+
+sql_rating = "INSERT INTO customers () VALUES (%s, %s)"
+val_rating = ("John", "Highway 21")
+db.execute(sql_rating, val_rating)
+
+db.commit()
